b2dea826be4c1510c548483025b38315fea56757
Florian lys <florianlys@MacBook-Pro-de-Florian.local>
2021-10-15 17:21:36 +0200

    agedBrie

--- test/texttest_fixture.js
+++ test/texttest_fixture.js
@@ -1,10 +1,11 @@
 
+const AgedBrie = require("../src/agedBrie");
 const { Shop, Item } = require("../src/gilded_rose");
 
 const items = [
   new Item("+5 Dexterity Vest", 10, 20),
-  new Item("Aged Brie", 2, 0),
   new Item("Elixir of the Mongoose", 5, 7),
+  new AgedBrie(2, 0),
   new Item("Sulfuras, Hand of Ragnaros", 0, 80),
   new Item("Sulfuras, Hand of Ragnaros", -1, 80),
   new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),

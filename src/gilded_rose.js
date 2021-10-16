eca0cb1c86b55d49e5b723edf3d30a965ff20305
Florian Borie <florian.borie@ynov.com>
2021-10-16 11:33:05 +0200

    refactor final

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -27,14 +27,13 @@ class Shop {
         this.items[i].sellIn = this.items[i].sellIn - 1;
       }
       if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            this.updateSulfuras(this.items[i])
-          } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality;
-          }
-        } else {
+        if (this.items[i].name == 'Aged Brie') {
           this.update(this.items[i])
+        } 
+        if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
+          this.descrease(this.items[i])
+        } else {
+          this.updateSulfuras(this.items[i])
         }
       }
     }
@@ -44,8 +43,16 @@ class Shop {
   
   update(item) {
     if (item.quality < 50) {
-      item.quality = item.quality + 1;
+      this.increase(item)
     }
+  }
+
+  increase(item) {
+    item.quality = item.quality + 1;
+  }
+
+  descrease(item) {
+    item.quality = item.quality - 1;
   }
 
   updateBackstage(item) {
@@ -57,7 +64,7 @@ class Shop {
 
   updateSulfuras(item) {
     if (item.quality > 0) {
-      item.quality = item.quality - 1;
+      this.descrease(item)
     }
   }
 

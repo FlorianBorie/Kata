bfc37839826d49f0369c684bbff098afb342b877
Florian Borie <florianborie@MacBook-Pro-de-Florian.home>
2021-10-16 19:06:20 +0200

    refactor final

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -10,66 +10,79 @@ class Shop {
   constructor(items=[]){
     this.items = items;
   }
+  
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name == 'Aged Brie') {
-        this.update(this.items[i])
+      switch(this.items[i].name) {
+        case 'Aged Brie': 
+          this.items[i] = this.updateAgedBrie(this.items[i])
+          break;
+        case 'Backstage passes to a TAFKAL80ETC concert': 
+          this.items[i] = this.updateBackstage(this.items[i])
+          break;
+        case 'Sulfuras, Hand of Ragnaros': 
+          this.items[i] = this.updateSulfuras(this.items[i])
+          break;
+        default:
+          this.items[i] = this.updateDexterity(this.items[i])
+          break;
       }
-      if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-        this.updateBackstage(this.items[i])
-      }
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        this.updateSulfuras(this.items[i])
-      } else {
-        this.update(this.items[i])
-      }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name == 'Aged Brie') {
-          this.update(this.items[i])
-        } 
-        if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-          this.descrease(this.items[i])
-        } else {
-          this.updateSulfuras(this.items[i])
-        }
-      }
+
     }
 
     return this.items;
   }
   
-  update(item) {
-    if (item.quality < 50) {
-      this.increase(item)
+  updateAgedBrie(item) {
+    item.sellIn = item.sellIn - 1
+    if (item.sellIn > 0) {
+      item.quality = item.quality + 1
+    } else {
+      item.quality = item.quality + 2
     }
-  }
-
-  increase(item) {
-    item.quality = item.quality + 1;
-  }
-
-  descrease(item) {
-    item.quality = item.quality - 1;
+    if (item.quality > 50) {
+      item.quality = 50
+    }
+    return item
   }
 
   updateBackstage(item) {
-    if (item.sellIn < 11 || item.sellIn < 6) {
-      this.update(item)
+    item.sellIn = item.sellIn - 1;
+    if (item.sellIn < 0) {
+      item.quality = 0;
     }
-
+    else if (item.sellIn <= 5) {
+      item.quality = item.quality + 3;
+    }
+    else if (item.sellIn <= 10) {
+      item.quality = item.quality + 2;
+    }
+    else {
+      item.quality = item.quality + 1;
+    }
+    if (item.quality > 50) {
+      item.quality = 50
+    }
+    return item
   }
 
   updateSulfuras(item) {
-    if (item.quality > 0) {
-      this.descrease(item)
-    }
+    return item
   }
 
-
-
+  updateDexterity(item) {
+    item.sellIn = item.sellIn - 1;
+    if (item.sellIn > 0) {
+      item.quality = item.quality - 1
+    }
+    else {
+      item.quality = item.quality - 2
+    }
+    if (item.quality > 50) {
+      item.quality = 50
+    }
+    return item
+  }
 }
 
 module.exports = {

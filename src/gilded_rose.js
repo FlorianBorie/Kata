e5f7e23d04615fd8bd7b5fbf6049e6c96a06e1aa
Florian lys <florianlys@MacBook-Pro-de-Florian.home>
2021-10-17 19:04:00 +0200

    Modification Dexterity

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -24,26 +24,11 @@ class Shop {
           item.update()
           break;
         default:
-          item = this.updateDexterity(item)
+          item.update()
           break;
       }
     })
     return this.items;
-  }
-
-
-  updateDexterity(item) {
-    item.sellIn = item.sellIn - 1;
-    if (item.sellIn > 0) {
-      item.quality = item.quality - 1
-    }
-    else {
-      item.quality = item.quality - 2
-    }
-    if (item.quality > 50) {
-      item.quality = 50
-    }
-    return item
   }
 }
 
--- test/ItemsClass/dexterity.js
+++ test/ItemsClass/dexterity.js
@@ -4,6 +4,20 @@ class Dexterity extends Item {
   constructor(sellIn, quality) {
     super('+5 Dexterity Vest', sellIn, quality)
   }
+
+  updateDexterity(item) {
+    item.sellIn = item.sellIn - 1;
+    if (item.sellIn > 0) {
+      item.quality = item.quality - 1
+    }
+    else {
+      item.quality = item.quality - 2
+    }
+    if (item.quality > 50) {
+      item.quality = 50
+    }
+    return item
+  }
 }
 
 module.exports = Dexterity;

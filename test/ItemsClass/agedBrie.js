60d6951125e3e3b7f97e2941918f605821337ad4
Florian lys <florianlys@MacBook-Pro-de-Florian.home>
2021-10-17 19:23:11 +0200

    refacto itemsclass agedbrie

--- test/ItemsClass/agedBrie.js
+++ test/ItemsClass/agedBrie.js
@@ -7,15 +7,8 @@ class AgedBrie extends Item {
   }
   update() {
     this.sellIn = this.sellIn - 1
-    if (this.sellIn > 0) {
-      this.quality = this.quality + 1
-    }
-    else {
-      this.quality = this.quality + 2
-    }
-    if (this.quality > 50) {
-      this.quality = 50
-    }
+    this.quality = this.sellIn > 0 ? this.quality += 1 : this.quality += 2
+    if (this.quality > 50) this.quality = 50
   }
 
 }

95cdbec7878ade54f9cf369c6e1e1cba449f8010
Florian lys <florianlys@MacBook-Pro-de-Florian.home>
2021-10-17 19:24:05 +0200

    refacto itemsclass backstage

--- test/ItemsClass/backstage.js
+++ test/ItemsClass/backstage.js
@@ -7,22 +7,12 @@ class Backstage extends Item {
 
     updateBackstage(item) {
       item.sellIn = item.sellIn - 1;
-      if (item.sellIn < 0) {
-        item.quality = 0;
-      }
-      else if (item.sellIn <= 5) {
-        item.quality = item.quality + 3;
-      }
-      else if (item.sellIn <= 10) {
-        item.quality = item.quality + 2;
-      }
-      else {
-        item.quality = item.quality + 1;
-      }
-      if (item.quality > 50) {
-        item.quality = 50
-      }
-      return item
+      if (this.sellIn < 0) this.quality = 0;
+      if (this.sellIn <= 5) this.quality += 3;
+      if (this.sellIn <= 10) this.quality += 2;
+      else this.quality += 1
+      if (this.quality > 50) this.quality = 50;
+  
     }
   }
 

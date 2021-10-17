2d92e4871967feb862ad8a99cd484ef17283de47
FlorianBorie <florian.borie@ynov.com>
2021-10-17 21:33:37 +0200

    update refactor final

--- test/ItemsClass/dexterity.js
+++ test/ItemsClass/dexterity.js
@@ -4,6 +4,13 @@ class Dexterity extends Item {
   constructor(sellIn, quality) {
     super('+5 Dexterity Vest', sellIn, quality)
   }
+
+  update(){    
+    this.sellIn = this.sellIn - 1;
+    this.quality = (this.sellIn > 0) ? this.quality -= 1 : this.quality -= 2
+    if (this.quality > 50) this.quality = 50
+
+  }
 }
 
 module.exports = Dexterity;

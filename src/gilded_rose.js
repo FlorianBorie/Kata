8b2e95050e3ddbb4631ffec23020750832a93e7d
Florian lys <florianlys@MacBook-Pro-de-Florian.home>
2021-10-17 18:46:29 +0200

    modification backstage

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -18,7 +18,7 @@ class Shop {
           item.update()
           break;
         case 'Backstage passes to a TAFKAL80ETC concert': 
-          item = this.updateBackstage(item)
+          item.update()
           break;
         case 'Sulfuras, Hand of Ragnaros':
           item = this.updateSulfuras(item)
@@ -29,26 +29,6 @@ class Shop {
       }
     })
     return this.items;
-  }
-
-  updateBackstage(item) {
-    item.sellIn = item.sellIn - 1;
-    if (item.sellIn < 0) {
-      item.quality = 0;
-    }
-    else if (item.sellIn <= 5) {
-      item.quality = item.quality + 3;
-    }
-    else if (item.sellIn <= 10) {
-      item.quality = item.quality + 2;
-    }
-    else {
-      item.quality = item.quality + 1;
-    }
-    if (item.quality > 50) {
-      item.quality = 50
-    }
-    return item
   }
 
   updateSulfuras(item) {
--- test/ItemsClass/backstage.js
+++ test/ItemsClass/backstage.js
@@ -3,7 +3,29 @@ const { Item } = require("../../src/gilded_rose");
 class Backstage extends Item {
   constructor(sellIn, quality) {
     super('Backstage passes to a TAFKAL80ETC concert' , sellIn, quality)
+    }
+
+    updateBackstage(item) {
+      item.sellIn = item.sellIn - 1;
+      if (item.sellIn < 0) {
+        item.quality = 0;
+      }
+      else if (item.sellIn <= 5) {
+        item.quality = item.quality + 3;
+      }
+      else if (item.sellIn <= 10) {
+        item.quality = item.quality + 2;
+      }
+      else {
+        item.quality = item.quality + 1;
+      }
+      if (item.quality > 50) {
+        item.quality = 50
+      }
+      return item
+    }
   }
-}
+
+
 
 module.exports = Backstage;
